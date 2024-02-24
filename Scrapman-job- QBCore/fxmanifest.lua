a7a207d725fd558e99793b83b9810f359c2d4419
ArielZ <74502022+ArielZ123@users.noreply.github.com>
2024-02-24 21:19:41 +0200

    Update fxmanifest.lua

--- Scrapman-job- QBCore/fxmanifest.lua
+++ Scrapman-job- QBCore/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'ArielZ'
-description 'OverTrue RolePlay scrapman-job for esx, you can convert it if you want to other frameworks feel free to use the script but please dont sell the script.'
+description 'OverTrue RolePlay scrapman-job for qbcore, you can convert it if you want to other frameworks feel free to use the script but please dont sell the script.'
 version '1.0.0'
 
 client_scripts {
